afd15429b837b843cbcbcf9ac10b259d937a65f7
silospen <silospen@silospen.com>
2013-03-24 23:34:29 +0000

    No factory - Temp provider can create itself

--- beerTemp/src/com/silospen/beerTemp/provider/TempProvider.py
+++ beerTemp/src/com/silospen/beerTemp/provider/TempProvider.py
@@ -1,11 +1,16 @@
+import os
+import logging
+
 class TempProvider():
-    PATH_SKELETON = "/sys/bus/w1/devices/{0}/w1_slave"
+    PARENT_SENSOR_PATH = "/sys/bus/w1/devices/"
+    SENSOR_PATH = PARENT_SENSOR_PATH + "{0}/w1_slave"
+    LOG = logging.getLogger('TempProvider')
 
     def __init__(self, sensorId):
         self.sensorId = sensorId
 
     def getTemp(self):
-        with TempProvider._rawTempHandle(self.PATH_SKELETON.format(self.sensorId)) as sensorInput:
+        with TempProvider._rawTempHandle(self.SENSOR_PATH.format(self.sensorId)) as sensorInput:
             return self._parseRawTemp(sensorInput.read())
 
     def _parseRawTemp(self, sensorInput):
@@ -14,3 +19,27 @@ class TempProvider():
     @staticmethod
     def _rawTempHandle(sensorPath):
         return open(sensorPath)
+
+    #noinspection PyBroadException
+    @classmethod
+    def create(cls):
+        sensors = []
+        try:
+            sensorList = cls._listSensors()
+        except Exception as ex:
+            cls.LOG.error(ex)
+            return []
+
+        for sensor in sensorList:
+            sensors.append(TempProvider(sensor))
+        return sensors
+
+    @classmethod
+    def _listSensors(cls):
+        return os.listdir(cls.PARENT_SENSOR_PATH)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
--- beerTemp/test/unit/com/silospen/beerTemp/provider/TempProviderTest.py
+++ beerTemp/test/unit/com/silospen/beerTemp/provider/TempProviderTest.py
@@ -1,33 +1,53 @@
 import unittest
 import io
 from unittest import mock
+from unittest.mock import patch
 from src.com.silospen.beerTemp.provider.TempProvider import TempProvider
 
 class TempProviderTest(unittest.TestCase):
     def testGetTemp(self):
-        self._setupExpectation("29 00 4b 46 ff ff 08 10 eb : crc=eb YES\n29 00 4b 46 ff ff 08 10 eb t=20250")
+        self._setupRawTempExpectation("29 00 4b 46 ff ff 08 10 eb : crc=eb YES\n29 00 4b 46 ff ff 08 10 eb t=20250")
         tempProvider = TempProvider("test_serial")
         self.assertEqual(20.25, tempProvider.getTemp())
         TempProvider._rawTempHandle.assert_called_once_with("/sys/bus/w1/devices/test_serial/w1_slave")
 
     def testGetTemp_withEmptyTempString(self):
-        self._setupExpectation("")
+        self._setupRawTempExpectation("")
         tempProvider = TempProvider("test_serial")
         self.assertRaises(Exception, tempProvider.getTemp)
         TempProvider._rawTempHandle.assert_called_once_with("/sys/bus/w1/devices/test_serial/w1_slave")
 
     def testGetTemp_withMissingTempString(self):
-        self._setupExpectation("29 00 4b 46 ff ff 08 10 eb : crc=eb YES\n29 00 4b 46 ff ff 08 10 eb t=")
+        self._setupRawTempExpectation("29 00 4b 46 ff ff 08 10 eb : crc=eb YES\n29 00 4b 46 ff ff 08 10 eb t=")
         tempProvider = TempProvider("test_serial")
         self.assertRaises(Exception, tempProvider.getTemp)
         TempProvider._rawTempHandle.assert_called_once_with("/sys/bus/w1/devices/test_serial/w1_slave")
 
     def testGetTemp_withBustedTempString(self):
-        self._setupExpectation("29 00 4b 46 ff ff 08 10 eb : crc=eb Y")
+        self._setupRawTempExpectation("29 00 4b 46 ff ff 08 10 eb : crc=eb Y")
         tempProvider = TempProvider("test_serial")
         self.assertRaises(Exception, tempProvider.getTemp)
         TempProvider._rawTempHandle.assert_called_once_with("/sys/bus/w1/devices/test_serial/w1_slave")
 
-    def _setupExpectation(self, expectedTempString):
+    def _setupRawTempExpectation(self, expectedTempString):
         TempProvider._rawTempHandle = mock.Mock(return_value=io.StringIO(expectedTempString))
 
+    @patch('src.com.silospen.beerTemp.provider.TempProvider.TempProvider._listSensors',
+        mock.Mock(return_value=["test_serial"]))
+    def testCreate_singleProvider(self):
+        self.assertEqual([TempProvider("test_serial")], TempProvider.create())
+
+    @patch('src.com.silospen.beerTemp.provider.TempProvider.TempProvider._listSensors',
+        mock.Mock(return_value=["test_serial1", "test_serial2", "test_serial3"]))
+    def testCreate_multipleProviders(self):
+        self.assertEqual([TempProvider("test_serial1"), TempProvider("test_serial2"), TempProvider("test_serial3")],
+            TempProvider.create())
+
+    @patch('src.com.silospen.beerTemp.provider.TempProvider.TempProvider._listSensors', mock.Mock(return_value=[]))
+    def testCreate_emptyList(self):
+        self.assertEqual([], TempProvider.create())
+
+    @patch('src.com.silospen.beerTemp.provider.TempProvider.TempProvider._listSensors',
+        mock.Mock(side_effect=FileNotFoundError))
+    def testCreate_withException(self):
+        self.assertEqual([], TempProvider.create())

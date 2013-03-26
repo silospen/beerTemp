d216874efc1525d4300c680dc380e9a81892d3dd
silospen <silospen@silospen.com>
2013-03-26 21:30:54 +0000

    Adding GPIO to heating element

--- beerTemp/src/com/silospen/beerTemp/heater/HeatingElement.py
+++ beerTemp/src/com/silospen/beerTemp/heater/HeatingElement.py
@@ -1,13 +1,26 @@
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError as e:
+    GPIO = None
+
 class HeatingElement():
+    ELEMENT_GPIO = 11
 
     def __init__(self):
+        if GPIO:
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.ELEMENT_GPIO, GPIO.OUT, initial=GPIO.HIGH)
         self.active = False
 
     def isActive(self):
         return self.active
 
     def activate(self):
+        if GPIO:
+            GPIO.output(self.ELEMENT_GPIO, GPIO.LOW)
         self.active = True
 
     def deactivate(self):
+        if GPIO:
+            GPIO.output(self.ELEMENT_GPIO, GPIO.HIGH)
         self.active = False
